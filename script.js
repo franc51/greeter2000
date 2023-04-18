c4e2c30fcc0e8f030865d5051d80fb649fef147d
franc51 <101558212+franc51@users.noreply.github.com>
2023-04-18 16:34:24 +0300

    started

---
+++ script.js
@@ -0,0 +1,22 @@
+let button = document.getElementById("btn");
+
+button.addEventListener("click", () => {
+    let fname = document.getElementById("f-name").value;
+    let lname = document.getElementById("l-name").value;
+    let age = document.getElementById("age").value;
+
+    console.log(fname);
+    console.log(lname);
+    console.log(age);
+    let body = document.getElementsByTagName('body')[0];
+
+    let newdiv = document.createElement("div");
+    newdiv.classList.add("greeter");
+    newdiv.style.display = "block";
+
+    let container = document.getElementById("container");
+    container.style.display = "none";
+    body.appendChild(newdiv);
+    let text = document.createTextNode("Hello " + fname + " " + lname + " how are you today?");
+    newdiv.appendChild(text);
+})
